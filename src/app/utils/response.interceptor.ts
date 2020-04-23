4a8f960728666f962b567340a8090cca34825b36
Arnab RAY <arnaray6@in.ibm.com>
2020-04-23 19:36:49 +0530

    Modified response.interceptor.ts file

--- src/app/utils/response.interceptor.ts
+++ src/app/utils/response.interceptor.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse, HttpResponseBase } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
-import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators'
 import { MessageBox, MessageBoxButton } from '../shared/message-box';
 import { MatDialog } from '@angular/material';
 
@@ -11,28 +9,30 @@ import { MatDialog } from '@angular/material';
 export class ResponseInterceptor implements HttpInterceptor {
 
     constructor(
-        private router: Router,
         private dialog: MatDialog,
     ) {
     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        return next.handle(request).do((event: HttpEvent<any>) => {
-            if (event instanceof HttpResponse) {
-            }
-        }, (err: any) => {
-            let errorMsg = '';
-            if (typeof err.error !== 'undefined')
-                errorMsg = err.error.message;
-            else if (typeof err.message !== 'undefined')
-                errorMsg = err.message;
-            else
-                errorMsg = err.error;
-
-            MessageBox.show(this.dialog, "Error", errorMsg, MessageBoxButton.Ok, "350px");
-
-        });
+        return next.handle(request).pipe(
+            tap((event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                }
+            }, (err: any) => {
+                let errorMsg = '';
+                if (typeof err.error !== 'undefined')
+                    errorMsg = err.error.message;
+                else if (typeof err.message !== 'undefined')
+                    errorMsg = err.message;
+                else
+                    errorMsg = err.error;
+                //console.log('errorMsg ' + errorMsg);
+                if (errorMsg == undefined)
+                    errorMsg = 'Something went wrong';
+                MessageBox.show(this.dialog, "Error", errorMsg, MessageBoxButton.Ok, "350px");
+            })
+        );
     }
 
 }
